fba98fcddd45369ad1bee37eef2de3aa83a8dfa2
SirTLD <ekanemita@gmail.com>
2021-06-20 01:14:15 -0500

    changed landing image

--- script.js
+++ script.js
@@ -6,14 +6,24 @@ const navLinkItems = document.querySelectorAll('.nav-links li');
 
 burgerIcon.addEventListener('click', () => {
   navbarLinks.classList.toggle('nav-active');
-
-  navLinkItems.forEach((link, index) => {
-    if (link.style.animation) {
-      link.style.animation = '';
-    } else {
-      link.style.animation = `menuSlide 1s ease forwards ${index / 5 + 0.4}s`;
-    }
-  });
-
   burgerIcon.classList.toggle('toggle-close');
+});
+
+const arrowLeft = document.querySelector('.fa-chevron-left');
+const arrowRight = document.querySelector('.fa-chevron-right');
+
+const divContainer = document.querySelector('.hero-title');
+
+const headerImage = document.querySelector('.header');
+
+const headingText = document.querySelector('.main-text');
+
+const bodyText = document.querySelector('.body-text');
+
+arrowRight.addEventListener('click', () => {
+  headerImage.classList.add('header-1');
+});
+
+arrowLeft.addEventListener('click', () => {
+  headerImage.classList.remove('header-1');
 });
